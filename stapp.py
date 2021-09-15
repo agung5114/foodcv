0d25f082dca625b867c050884044c7065f237e6e
agung5114 <43102810+agung5114@users.noreply.github.com>
2021-09-15 17:51:05 +0700

    Add files via upload

--- stapp.py
+++ stapp.py
@@ -21,10 +21,30 @@ sys.modules['Image'] = Image
 base="light"
 primaryColor="purple"
 
-model = keras.models.load_model('foodclass.h5')
+model = keras.models.load_model('fcvmodel.h5')
+dbfood = pd.read_csv('dbfood.csv',sep=";")
+food = dbfood['nama'].tolist()
+# def getPrediction(path):
+#     img = Image.open(path)
+#     newsize = (224, 224)
+#     image = img.resize(newsize)
+#     image = img_to_array(image)
+#     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+#     image = preprocess_input(image)
+#     yhat = model.predict(image)
+#     label = yhat[0]
+#     # a,b,c,d = np.round(label[0]*100,2),np.round(label[1]*100,2),np.round(label[2]*100,2),np.round(label[3]*100,2)
+#     # top = dict(zip(['bakso','pempek','sate','soto'], [a,b,c,d]))
+#     # a,b,c,d = np.round(label[0]*100,2),np.round(label[1]*100,2),np.round(label[2]*100,2),np.round(label[3]*100,2)
+#     prob = []
+#     for i in range(len(label)):
+#         prob.append(np.round(label[i]*100,2))
+#     top = dict(zip(['Ayam','Bakso','Bubur_Ayam','Gado_Gado','Gudeg','Hamburger','Kentang_Goreng','Ketoprak','Mie_Goreng','Nasi_Goreng','Pempek','Rawon','Sate','Sayur_Asam','Siomay','Soto','Tongseng'], prob))
+#     top3 = dict(sorted(top.items(), key=operator.itemgetter(1), reverse=True)[:3])
+#     return top3
 
-def getPrediction(path):
-    img = Image.open(path)
+def getPrediction(data,model):
+    img = Image.open(data)
     newsize = (224, 224)
     image = img.resize(newsize)
     image = img_to_array(image)
@@ -32,41 +52,43 @@ def getPrediction(path):
     image = preprocess_input(image)
     yhat = model.predict(image)
     label = yhat[0]
-    # a,b,c,d = np.round(label[0]*100,2),np.round(label[1]*100,2),np.round(label[2]*100,2),np.round(label[3]*100,2)
-    # top = dict(zip(['bakso','pempek','sate','soto'], [a,b,c,d]))
-    # a,b,c,d = np.round(label[0]*100,2),np.round(label[1]*100,2),np.round(label[2]*100,2),np.round(label[3]*100,2)
     prob = []
     for i in range(len(label)):
         prob.append(np.round(label[i]*100,2))
-    top = dict(zip(['Ayam','Bakso','Bubur_Ayam','Gado_Gado','Gudeg','Hamburger','Kentang_Goreng','Ketoprak','Mie_Goreng','Nasi_Goreng','Pempek','Rawon','Sate','Sayur_Asam','Siomay','Soto','Tongseng'], prob))
+    top = dict(zip(food, prob))
     top3 = dict(sorted(top.items(), key=operator.itemgetter(1), reverse=True)[:3])
     return top3
 
 st.set_page_config(layout='wide')
 
 def main():
-    menu = ["Food Calorie Estimator","Profile","Recommendations"]
+    # menu = ["Food Calorie Estimator","Profile","Recommendations"]
     
-    choice = st.sidebar.selectbox("Select Menu", menu)
+    # choice = st.sidebar.selectbox("Select Menu", menu)
 
-        
-    if choice == "Food Calorie Estimator":
-        st.subheader("Food Calorie Estimator")
-        data = st.file_uploader('Upload Foto')
-        if data == None:
-            st.write('Silakan Upload Foto')
-        else:
-            st.image(data)
+    # if choice == "Food Calorie Estimator":
+    st.subheader("Food Calorie Estimator")
+    data = st.file_uploader('Upload Foto')
+    if data == None:
+        st.write('Silakan Upload Foto')
+    else:
+        st.image(data)
 
-        if st.button('Prediksi'):
-            hasil = getPrediction(data)
-            st.write(hasil)
+    if st.button('Prediksi'):
+        hasil = getPrediction(data,model)
+        # dfhasil = pd.DataFrame.from_dict(hasil)
+        keys = list(hasil.keys())
+        dbkal = dbfood[dbfood['nama'].isin(keys)]
+        # db = dbkal.join(dfhasil,how='left',on)
+        st.write(keys[0])
+        # st.write(dfhasil)
+        st.write(dbkal)
 
-    elif choice == "Profile":
-        st.title("Profile")
+    # elif choice == "Profile":
+    #     st.title("Profile")
 
-    elif choice == "Recommendations":
-        st.title("Recommendations")
+    # elif choice == "Recommendations":
+    #     st.title("Recommendations")
         
 
 if __name__=='__main__':
