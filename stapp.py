a6d82bdbb5598409c82a7e1773fe87703c26b56f
agung5114 <43102810+agung5114@users.noreply.github.com>
2021-09-18 13:15:19 +0700

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -91,6 +91,8 @@ def main():
             risiko = 'Aman'
         top1 = top['nama'].tolist()
         st.subheader(top1[0])
+        out = '''<h3>f'{str(top1[0]}'<h3>'''
+        st.markdown(out, unsafe_allow_html=True)
         st.write(f'Risiko bagi penderita Diabetes/Jantung: {risiko}')
         # st.write(dfk)
         a = dfk['Kkal'].sum()
